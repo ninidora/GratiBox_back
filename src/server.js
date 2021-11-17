57a1df6c164b0378788947fba772a463d1711797
thipereira02 <thiago.spereira02@gmail.com>
2021-11-17 11:03:31 -0300

    Installing and setting eslint

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
-import "./setup.js"; 
-import app from "./app.js";
+/* eslint-disable no-console */
+import './setup.js';
+import app from './app.js';
 
 const port = Number(process.env.PORT);
 
 app.listen(port, () => {
-	console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
