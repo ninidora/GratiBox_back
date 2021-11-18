8c4664f821c16a2cdffcc7ad39122cf076eb569b
thipereira02 <thiago.spereira02@gmail.com>
2021-11-18 11:28:28 -0300

    Creating routes of sign up and login

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 
+import * as userController from './controllers/userController.js';
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.post('/signup', userController.signUp);
+app.post('/login', userController.signIn);
 
 export default app;
